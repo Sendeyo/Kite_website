e148175aec19d76bfca79677ae1fd674925bcbd0
Sendeyo <maxmuthomi@gmail.com>
2019-12-20 10:42:08 +0300

    Time fix

--- fun.py
+++ fun.py
@@ -5,13 +5,20 @@ from dateutil.parser import parse
 
 def Convert(date):
     dt = parse(date)
-    dt + timedelta(hours=9)
+    dt = dt + timedelta(hours=3)
     times =dt.strftime("%b %d %Y %H:%M:%S")
     return times
     
 
 def Convert4User(date):
     dt = parse(date)
-    dt + timedelta(hours=9)
-    times =dt.strftime("%b %d - %Y at %H:%M:%S")
+    dt = dt + timedelta(hours=3)
+    times =dt.strftime("%b %d - %Y at %H :%M:%S")
     return times
+
+def TimeJoke():
+    t = time.time()
+    print(t)
+
+
+# TimeJoke()
